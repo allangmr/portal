8f10bd46f36f0fe9eb238c1cebc617bf99853169
Allan Montilla <allangmr7@gmail.com>
2019-10-14 18:13:57 -0500

    No procede el commit.
    Listo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Bienvenido Nuevamente!</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Bienvenido Nuevamente al Sistema!</h1>
                   </div>
 
                   <form method="POST"  class="user" action="{{ route('login') }}">
